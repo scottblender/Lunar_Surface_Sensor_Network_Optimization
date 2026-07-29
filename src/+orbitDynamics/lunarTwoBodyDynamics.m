891cfb9aafce73d3c20daa423bb1edbc0d08d5d7
scottblender <scottblender20@gmail.com>
2026-07-29 18:22:59 -0400

    removed comments

--- src/+orbitDynamics/lunarTwoBodyDynamics.m
+++ src/+orbitDynamics/lunarTwoBodyDynamics.m
@@ -12,13 +12,10 @@ arguments
     moonMu (1,1) double {mustBePositive} = 4902.800066
 end
 
-% Extract position and velocity from the state vector
 position = state(1:3);
 velocity = state(4:6);
 
-% Calculate the acceleration due to lunar gravity
-r = norm(position); % Distance from the moon's center
+r = norm(position);
 acceleration = -moonMu * position / r^3;
 
-% Combine velocity and acceleration into the state derivative
 stateDerivative = [velocity; acceleration];
